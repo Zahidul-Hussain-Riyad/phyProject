063fdf897d89445b2234dc2e2c8925c5dcc7a0d9
Zahidul-Hussain-Riyad <zahidulhussain900@gmail.com>
2025-03-28 23:32:03 +0600

    Add files via upload

---
+++ Problem - 2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ( )
+{
+    float lambda1 = 530;
+    float lambda2 = 700;
+    float theta1 = 65.0;
+    float a, theta2;
+
+    a = (3 * lambda1) / sin(theta1 * (3.1416 / 180));
+    theta2 = asin(2 * lambda2 / a) * (180 / 3.1416);
+
+    printf("The second-order bright spot for red light is at %.2f degrees.\n", theta2);
+
+    return 0;
+}
+
